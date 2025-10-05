38d8f0d2b4ba404e4f9791be76d85053ec3b3e48
Ilesandres <ilesandres6@gmail.com>
2025-10-04 21:45:18 -0500

    lint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 function sum(a, b) {
-	return a + b;
+  return a + b;
 }
 
 if (require.main === module) {
-	console.log('hola mundo');
-	console.log('2 + 3 =', sum(2, 3));
+  console.log('hola mundo');
+  console.log('2 + 3 =', sum(2, 3));
 }
 
 module.exports = { sum };
--- node.test.js
+++ node.test.js
@@ -1,10 +1,10 @@
 const { sum } = require('./index');
 
 test('sum adds two numbers', () => {
-	expect(sum(1, 2)).toBe(3);
-	expect(sum(-1, 1)).toBe(0);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(-1, 1)).toBe(0);
 });
 
 test('sum handles zeros', () => {
-	expect(sum(0, 0)).toBe(0);
+  expect(sum(0, 0)).toBe(0);
 });
