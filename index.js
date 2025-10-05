56f6590cec8033c452abf5618636688fe6b3be5d
Ilesandres <ilesandres6@gmail.com>
2025-10-04 21:14:54 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola mundo')
+
+function sum(a, b) {
+	return a + b;
+}
+
+if (require.main === module) {
+	console.log('hola mundo');
+	console.log('2 + 3 =', sum(2, 3));
+}
+
+module.exports = { sum };
